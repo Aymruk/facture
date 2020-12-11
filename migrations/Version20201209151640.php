72e074fd17f4c7fe057efd8e2db8f956905843da
Aymruk <aymeric.painvin@gmail.com>
2020-12-11 11:46:15 +0100

    prjet

---
+++ migrations/Version20201209151640.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20201209151640 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE facture (id INT AUTO_INCREMENT NOT NULL, numero_facture INT NOT NULL, id_emetteur INT NOT NULL, id_recepteur INT NOT NULL, montant_ttc DOUBLE PRECISION NOT NULL, montant_ht DOUBLE PRECISION NOT NULL, date_facture DATE NOT NULL, date_inpi DATE NOT NULL, alerte INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE facture');
+    }
+}
